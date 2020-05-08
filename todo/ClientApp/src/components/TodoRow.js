9c569fdc2add377ddc9ac0559a32e0edcb4459fd
Rafael Pimentel <rafyraf08@gmail.com>
2020-05-08 03:17:27 -0400

    Add project files.

---
+++ todo/ClientApp/src/components/TodoRow.js
@@ -0,0 +1,12 @@
+﻿import React, { Component } from 'react'
+
+export class TodoRow extends Component {
+
+    render = () =>
+        <tr key={this.props.item.action}>
+            <td>{this.props.item.action}</td>
+            <td for="done">
+                <input id="done" type="checkbox" checked={this.props.item.done} onChange={() => this.props.callback(this.props.item)} />
+            </td>
+        </tr>
+}
---
+++ todo/ClientApp/src/components/VisibilityControl.js
@@ -0,0 +1,14 @@
+﻿import React, { Component } from "react"
+
+export class VisibilityControl extends Component {
+    render = () =>
+        <div className="form-check">
+            <label className="form-check-label">
+                <input className="form-check-input"
+                    type="checkbox"
+                    checked={this.props.isChecked}
+                    onChange={(e) => this.props.callback(e.target.checked)} />
+                Show {this.props.description} ({this.props.tasks.filter(t => t.done).length})
+            </label>
+        </div>
+}
